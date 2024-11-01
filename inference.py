2471c142bc9261ece343ba55faa856c450ddf119
Nam Doan Ngoc <namdng@coccoc.com>
2024-11-01 15:25:10 +0700

    add benchmark to inference script

--- inference.py
+++ inference.py
@@ -1,3 +1,4 @@
+import os
 import time
 
 import torch
@@ -8,6 +9,7 @@ from torchvision import transforms
 
 MODEL_PATH = "/home/namdng/garbage_classfier/models/init/ckpt_10_0.9331.pth"
 IMAGE_SIZE = 394
+BENCHMARK = True
 
 ### Device
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -38,8 +40,8 @@ model_conv.eval()
 
 ### Inference
 def visualize_model_predictions(model,img_path):
-    since = time.time()
     model.eval()
+    since = time.time()
 
     img = Image.open(img_path)
     img = data_transforms['test'](img)
@@ -49,12 +51,25 @@ def visualize_model_predictions(model,img_path):
     with torch.no_grad():
         outputs = model(img)
         _, preds = torch.max(outputs, 1)
-        print(f'Predicted: {class_names[preds[0]]}')
     
-    print(f'Inference time: {time.time() - since}')
+    return class_names[preds[0]], time.time() - since
 
-visualize_model_predictions(
-    model_conv,
-    img_path='/home/namdng/garbage_classfier/data_split/test/cardboard_paper/cardboard118_jpg.rf.ba1dfe15a031c688b3e95d08645d97ce.jpg'
-)
+
+TEST_DIR = 'data_split/test/others'
+test_images_name = [item for item in os.listdir(TEST_DIR) if item.endswith('.jpg')]
+print(f"Total test images: {len(test_images_name)}")
+
+if BENCHMARK:
+    time_array = []
+    for img_name in test_images_name:
+        img_path = os.path.join(TEST_DIR, img_name)
+        pred, time_elapsed = visualize_model_predictions(model_conv, img_path)
+        time_array.append(time_elapsed)
+
+    print(f"Average inference time: {sum(time_array) / len(time_array)}")
+else:
+    # pred, time_elapsed = visualize_model_predictions(model_conv, os.path.join(TEST_DIR, test_images_name[0]))
+    pred, time_elapsed = visualize_model_predictions(model_conv, "data_split/test/others/trash518_data1.jpg")
+    print(f"Predicted class: {pred}")
+    print(f"Inference time: {time_elapsed}")
 
