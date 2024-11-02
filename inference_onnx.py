a7e48d168b960cb7965cbbdd276da636c49e98f2
Nam Doan <namdng@coccoc.com>
2024-11-02 09:43:07 +0700

    add onnx optimization

--- inference_onnx.py
+++ inference_onnx.py
@@ -4,18 +4,22 @@ import time
 import torch
 import torch.nn as nn
 import torchvision
+import onnxruntime as rt
 from PIL import Image
 from torchvision import transforms
 from tqdm import tqdm
 
-MODEL_PATH = "/home/namdng/garbage_classfier/models/init/ckpt_26_0.9547.pth"
+# set num threads to 1 (NOTE: disable this when on Raspberry Pi)
+sess_opt = rt.SessionOptions()
+sess_opt.intra_op_num_threads = 1
+
+MODEL_PATH = "/home/namdng/garbage_classifier/models/init/gc_opset20.onnx"
 IMAGE_SIZE = 394
 BENCHMARK = True
 
-# torch.set_num_threads(1)
-
 ### Device
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+# device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+device = "cpu"
 
 ### Data transforms
 data_transforms = {
@@ -28,22 +32,11 @@ data_transforms = {
 
 ### Load model
 class_names = ["cardboard_paper", "glass", "metal", "others", "plastic"]
-model_conv = torchvision.models.efficientnet_v2_s()
-num_ftrs = model_conv.classifier[1].in_features
-model_conv.classifier = nn.Sequential(
-    nn.Dropout(0.2),
-    nn.Linear(num_ftrs, 128),
-    nn.ReLU(),
-    nn.Dropout(0.2),
-    nn.Linear(128, len(class_names))
-)
-model_conv = model_conv.to(device)
-model_conv.load_state_dict(torch.load(MODEL_PATH, weights_only=True))
-model_conv.eval()
+ort_session = rt.InferenceSession(MODEL_PATH, sess_opt, providers=["CPUExecutionProvider"])
+
 
 ### Inference
-def visualize_model_predictions(model,img_path):
-    model.eval()
+def visualize_model_predictions(ort_session,img_path):
     since = time.time()
 
     img = Image.open(img_path)
@@ -51,9 +44,12 @@ def visualize_model_predictions(model,img_path):
     img = img.unsqueeze(0)
     img = img.to(device)
 
-    with torch.no_grad():
-        outputs = model(img)
-        _, preds = torch.max(outputs, 1)
+    def to_numpy(tensor):
+        return tensor.detach().cpu().numpy() if tensor.requires_grad else tensor.cpu().numpy()
+    ort_inputs = {ort_session.get_inputs()[0].name: to_numpy(img)}
+    ort_outs = ort_session.run(None, ort_inputs)
+    outputs = torch.tensor(ort_outs[0])
+    _, preds = torch.max(outputs, 1)
     
     return class_names[preds[0]], time.time() - since
 
@@ -65,19 +61,25 @@ for class_name in os.listdir(TEST_DIR):
     for img_name in os.listdir(class_dir):
         img_path = os.path.join(class_dir, img_name)
         test_images_paths.append(img_path)
+
+test_images_paths = test_images_paths[:200]
 print(f"Total test images: {len(test_images_paths)}")
 
 if BENCHMARK:
     time_array = []
     for img_path in tqdm(test_images_paths):
-        pred, time_elapsed = visualize_model_predictions(model_conv, img_path)
+        pred, time_elapsed = visualize_model_predictions(ort_session, img_path)
         time_array.append(time_elapsed)
 
     print(f"Average inference time: {sum(time_array) / len(time_array)}")
 else:
     # pred, time_elapsed = visualize_model_predictions(model_conv, os.path.join(TEST_DIR, test_images_name[0]))
-    pred, time_elapsed = visualize_model_predictions(model_conv, "data_split/test/others/trash518_data1.jpg")
+    pred, time_elapsed = visualize_model_predictions(ort_session, "data_split/test/others/battery_27_data2.jpg")
     print(f"Predicted class: {pred}")
     print(f"Inference time: {time_elapsed}")
 
-# Latency for GPU: 0.014438848256210272
+# Latency for CPU (i5-12500, 1 CPU core):
+#   0.2265708318320654 (torch model)
+#   0.17627952814102174 (onnx opset 10)
+#   0.1762681257724762 (onnx opset 20)
+
