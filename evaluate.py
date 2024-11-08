a51c25c8cc6585e471724d74940132602fe975dd
Nam Doan Ngoc <namdng@coccoc.com>
2024-11-08 11:49:21 +0700

    update evaluate.py to use load_model()

--- evaluate.py
+++ evaluate.py
@@ -10,6 +10,8 @@ import numpy as np
 from tqdm import tqdm
 
 from quantize_utils import static_quantize_model
+import hyperparams as hparams
+from model import load_model
 
 # Setting seed for all random initializations
 SEED = 2
@@ -17,13 +19,11 @@ random.seed(SEED)
 np.random.seed(SEED)
 torch.manual_seed(SEED)
 
-MODEL_PATH = "/home/namdng/garbage_classifier/models/lr_1e-3_bs_64_sche-f0.2-p6/ckpt_63_0.9641_.pth"
+MODEL_PATH = "/home/namdng/garbage_classifier/models/resnet50_tuned_lr_1e-3_bs_64_sche-f0.2-p6/ckpt_57_0.9597.pth"
 DATA_DIR = 'data_split'
 DATA_SPLITS = ['calibration', 'val', 'test']
 
-BATCH_SIZE = 32
-IMAGE_SIZE = 394
-QUANTIZE_MODEL = True
+BATCH_SIZE = 64
 
 ### Device
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -31,7 +31,7 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 ### Data transforms
 data_transforms = {
     x: transforms.Compose([
-        transforms.Resize((IMAGE_SIZE, IMAGE_SIZE)),
+        transforms.Resize((hparams.IMAGE_SIZE, hparams.IMAGE_SIZE)),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ]) for x in DATA_SPLITS
@@ -48,18 +48,10 @@ dataset_sizes = {x: len(image_datasets[x]) for x in DATA_SPLITS}
 class_names = image_datasets[DATA_SPLITS[0]].classes
 
 ### Load model
-model_conv = torchvision.models.efficientnet_v2_s()
-num_ftrs = model_conv.classifier[1].in_features
-model_conv.classifier = nn.Sequential(
-    nn.Dropout(0.2),
-    nn.Linear(num_ftrs, 128),
-    nn.ReLU(),
-    nn.Dropout(0.2),
-    nn.Linear(128, len(class_names))
-)
-model_conv = model_conv.to(device)
-print("Load model from", MODEL_PATH)
+print("Loading model: ", MODEL_PATH)
+model_conv = load_model(hparams.BACKBONE, hparams.NUM_IMMEDIATE_FEATURES, len(class_names), hparams.DROPOUT_RATE)
 model_conv.load_state_dict(torch.load(MODEL_PATH, weights_only=True))
+model_conv = model_conv.to(device)
 criterion = nn.CrossEntropyLoss()
 
 ### Evaluate
@@ -91,15 +83,4 @@ def evaluate_model(model, criterion):
     time_elapsed = time.time() - since
     print(f"Eval complete in {time_elapsed // 60:.0f}m {time_elapsed % 60:.0f}s")
 
-if QUANTIZE_MODEL:
-    device = "cpu"
-    model_conv = model_conv.to(device)
-    quantized_model = static_quantize_model(model_conv, dataloaders['calibration'], backend="fbgemm")
-    evaluate_model(quantized_model, criterion)
-
-    # enable this to save quantized model
-    # model_dir_name, model_base_name = os.path.dirname(MODEL_PATH), os.path.basename(MODEL_PATH)
-    # model_name = model_base_name.replace('.pth', '')
-    # torch.jit.save(torch.jit.script(quantized_model), f"{model_dir_name}/{model_name}_quantized.pt")
-else:
-    evaluate_model(model_conv, criterion)
+evaluate_model(model_conv, criterion)
