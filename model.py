8033c3972ad5cee19f4c2e2240c31b452cc0120b
Nam Doan Ngoc <namdng@coccoc.com>
2024-11-08 11:45:35 +0700

    use model torchvision.models.quantization.resnet50 in model.py

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
-import torchvision
 import torch.nn as nn
+from torchvision.models import efficientnet_v2_s
+from torchvision.models.quantization import resnet50
 
 def setup_classifier_head(
         num_ftrs: int,
@@ -22,13 +23,13 @@ def load_model(
         dropout_rate: float) -> nn.Module:
     
     if backbone == "efficientnet_v2s":
-        model_conv = torchvision.models.efficientnet_v2_s(weights='IMAGENET1K_V1')
+        model_conv = efficientnet_v2_s(weights='IMAGENET1K_V1')
         for param in model_conv.parameters():
             param.requires_grad = False
         num_ftrs = model_conv.classifier[1].in_features
         model_conv.classifier = setup_classifier_head(num_ftrs, num_immediate_features, num_class, dropout_rate)
     elif backbone == "resnet50":
-        model_conv = torchvision.models.resnet50(weights='IMAGENET1K_V1')
+        model_conv = resnet50(weights='IMAGENET1K_V1', quantize=False)
         for param in model_conv.parameters():
             param.requires_grad = False
         num_ftrs = model_conv.fc.in_features
